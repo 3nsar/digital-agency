b621bf5d7da537fcc5ec56b9c08c2de5445c959a
3nsar <84131267+3nsar@users.noreply.github.com>
2024-01-02 08:18:56 +0100

    commit

--- digital-agency/src/components/Hero.tsx
+++ digital-agency/src/components/Hero.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Hero.scss';
-import heroVideo from  "../video/hero-video.mp4"
+import heroVideo from  "../video/head.mp4"
 
 const Hero = () => {
   return (
     <div className='hero-container'>
        <video src={heroVideo} autoPlay loop muted  className='video-background' />
+       <div className='overlay'></div>
         <div className="hero-content">
-      <h1>Bringing your vision to its fullest potential</h1>
+      <h1>Bringing your<br /><span className='gradient-text'>vision</span> to its<br /> fullest potential</h1>
       </div>
     </div>
   )
