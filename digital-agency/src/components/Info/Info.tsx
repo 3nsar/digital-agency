d03f533bf75ed56a583d2d261a7f8dc9ece6e0b9
3nsar <84131267+3nsar@users.noreply.github.com>
2024-01-02 21:46:07 +0100

    commit

---
+++ digital-agency/src/components/Info/Info.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Info.scss"
+
+const Info = () => {
+  return (
+    <div className='info-container'>
+        <div className="info-content">
+            <div className="info-text">
+              <p className='small-text'>No fluff, no long processes, no bloated teams...</p>
+              <p>Streamlined design and development.</p>
+              <p>Swift and remarkable results.</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Info
