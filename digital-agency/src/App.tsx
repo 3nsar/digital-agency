12e5b596b552510c26f836b2fbe6dac7612d658d
3nsar <84131267+3nsar@users.noreply.github.com>
2024-01-03 08:24:12 +0100

    commit

--- digital-agency/src/App.tsx
+++ digital-agency/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css'
 import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 import Sponsor from './components/Sponsor/Sponsor'
+import Info from './components/Info/Info'
+import Cards from './components/Cards/Cards'
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <Sponsor/>
+      <Info/>
+      <Cards/>
     </div>
   )
 }
