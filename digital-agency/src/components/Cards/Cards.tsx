5b310fa42a7530ce08969a4c6010c393b9fd50cf
3nsar <84131267+3nsar@users.noreply.github.com>
2024-01-03 08:23:59 +0100

    commit
    added cards

--- digital-agency/src/components/Cards/Cards.tsx
+++ digital-agency/src/components/Cards/Cards.tsx
@@ -3,6 +3,7 @@ import "./Cards.scss"
 import { TbNorthStar } from "react-icons/tb";
 import pic1 from "../../images/web1.jpg"
 import pic2 from "../../images/web2.jpg"
+import { FaLocationDot } from "react-icons/fa6";
 
 const Cards = () => {
   return (
@@ -13,22 +14,22 @@ const Cards = () => {
             <div className="cards">
                 <img src={pic1}alt="pic" />
                 <div className="cards-details">
-                    <p>DELAWARE, USA</p>
+                    <p><FaLocationDot/> DELAWARE, USA</p>
                     <p>Cryptocurrency</p>
                 </div>
                 <p className='cards-title'>Deepwaters</p>
-                <p>Product Design</p>
+                <p className='cards-kind'>Product Design</p>
                 <button>View project</button>
             </div>
 
             <div className="cards">
                 <img src={pic2} alt="pic" />
                 <div className="cards-details">
-                    <p>DELAWARE, USA</p>
-                    <p>Cryptocurrency</p>
+                    <p><FaLocationDot/> LONDON, UK</p>
+                    <p>Finance</p>
                 </div>
-                <p className='cards-title'>Deepwaters</p>
-                <p>Product Design</p>
+                <p className='cards-title'>Grain</p>
+                <p className='cards-kind'>Web Build</p>
                 <button>View project</button>
             </div>
        </div>
