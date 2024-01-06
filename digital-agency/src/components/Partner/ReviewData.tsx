7df9c11be2214d336a19a10e842ccebe67da1ab5
3nsar <84131267+3nsar@users.noreply.github.com>
2024-01-06 23:08:04 +0100

    commit

---
+++ digital-agency/src/components/Partner/ReviewData.tsx
@@ -0,0 +1,27 @@
+const reviews= [
+  {
+    "text": "Delighted with the service we have received - always quick, efficient, and extremely helpful. Would definitely recommend!",
+    "author": "Jordan Coleman",
+    "company": "Founder, Hills Speed"
+  },
+  {
+    "text": "Exceptional service! The team is highly professional and goes above and beyond to meet our needs. I recommend their services to everyone.",
+    "author": "Alice Rodriguez",
+    "company": "Marketing Manager, Solana"
+  },
+  {
+    "text": "Outstanding support from start to finish. Quick response times and a friendly team that truly cares about their clients. Highly recommended!",
+    "author": "Michael Chen",
+    "company": "Co-Founder, Moon Health"
+  },
+  {
+    "text": "Impressed with the level of expertise and dedication. They consistently deliver top-notch service. I wouldn't hesitate to refer them to others.",
+    "author": "Emily Wilson",
+    "company": "Founder, Verra UK"
+  },
+  {
+    "text": "Professionalism at its best! The team at this company is reliable, efficient, and always ready to assist. I am a satisfied customer and will continue to work with them.",
+    "author": "Daniel James",
+    "company": "Co-Founder, Leeds & Co"
+  }
+]
