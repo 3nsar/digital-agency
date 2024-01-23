80259f39da7b70e6c406e93a7f77df1988fba1ae
3nsar <84131267+3nsar@users.noreply.github.com>
2024-01-23 19:23:28 +0100

    commit

--- digital-agency/src/components/Navbar/Navbar.tsx
+++ digital-agency/src/components/Navbar/Navbar.tsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './Navbar.scss';
 import { TbNorthStar } from "react-icons/tb";
+import { FaBars } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
 
 const Navbar = () => {
+
+  const [showNav, setShowNav] = useState<boolean>(false);
+  const [isSticky, setIsSticky] = useState<boolean>(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setIsSticky(true);
+    } else {
+      setIsSticky(false);
+    }
+  };
+
+  const toggleNav = () => {
+    setShowNav(!showNav);
+  };
+
+  const closeNav = () => {
+    setShowNav(false);
+  };
+
   return (
-    <div className='nav-container'>
+    <div className={`nav-container  ${isSticky ? 'sticky' : ''}`}>
       <div className="nav-title">
         <h1>Exo<TbNorthStar size="2rem" color="#ff0066"/>co</h1>
         </div>
-        <div className="list-content">
+        <div className={`list-content ${showNav ? 'show' : ''}`}>
             <ul>
                 <li>Blocks</li>
                 <li>Portfolio</li>
@@ -18,6 +40,10 @@ const Navbar = () => {
             </ul>
         </div>
         <button>Hire us</button>
+        <div className='nav-icon' onClick={toggleNav}>
+        {showNav ? <AiOutlineClose fontSize="25px" /> : <FaBars fontSize="25px" />}
+      </div>
+
     </div>
   )
 }
