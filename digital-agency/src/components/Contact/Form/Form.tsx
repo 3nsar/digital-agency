11a771b0acba3079e7dd1267c54c7e3065191b6c
3nsar <84131267+3nsar@users.noreply.github.com>
2024-01-23 19:23:39 +0100

    commit

---
+++ digital-agency/src/components/Contact/Form/Form.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useState } from 'react'
+import "./Form.scss"  
+
+const FormInput = (props:any) => {
+    const {label, errorMessage,onChange, id, ...inputProps} = props
+    const [focused, setFocused] = useState(false);
+
+    const handleFocus = ()=>{
+        setFocused(true)
+    }
+  return (
+    <div className='formInput'>
+       <label>{label}</label> 
+       <input {...inputProps} onChange={onChange} onBlur={handleFocus} focused={focused.toString()}/>
+       <span className='error-msg'>{errorMessage}</span>
+    
+    </div>
+  )
+}
+
+export default FormInput
