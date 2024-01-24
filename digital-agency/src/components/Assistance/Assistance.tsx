e0145c6c4aea034b71987b39ab98bf46d4b8d7fe
3nsar <84131267+3nsar@users.noreply.github.com>
2024-01-24 13:15:01 +0100

    commit

--- digital-agency/src/components/Assistance/Assistance.tsx
+++ digital-agency/src/components/Assistance/Assistance.tsx
@@ -30,8 +30,8 @@ const Assistance = () => {
         <div className="assistance-title">
           <p className='small-title'><TbNorthStar color="#0044ff"/> What we do</p>
           <h1>Choose all the service <span className='rainbow'>blocks</span><br />or just the ones you need...</h1>
-          <p className='assistance-title-text'>Tailored services <span className='dark-text'>without the agency fluff</span>: discovery, strategy, branding, website and</p>
-          <p className='assistance-title-text'>product design, Webflow development, and training, all in one comprehensive suite.</p>
+          <p className='assistance-title-text'>Tailored services <span className='dark-text'>without the agency fluff</span>: discovery, strategy, branding, website and
+             <br className='assistance-break'/>product design, Webflow development, and training, all in one comprehensive suite.</p>
         </div>
         <div className="assistance-options">
           <ul>
