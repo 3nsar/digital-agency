41e404fcf0ba10b3588eeaf22c6d97be290f7a7e
3nsar <84131267+3nsar@users.noreply.github.com>
2024-01-31 11:36:25 +0100

    commit

--- digital-agency/src/components/Hero/Hero.tsx
+++ digital-agency/src/components/Hero/Hero.tsx
@@ -1,7 +1,7 @@
 import './Hero.scss';
 import heroVideo from  "../../video/hero-video.mp4"
 import { TbNorthStar } from "react-icons/tb";
-
+import { motion } from 'framer-motion';
 
 
 
@@ -14,7 +14,11 @@ const Hero = () => {
         <div className="hero-subtitle">
           <p><TbNorthStar size="2rem"/>Design & development studio</p>
        </div>
-      <h1>Bringing your<br /><span className='gradient-text'>vision</span> to its<br /> fullest potential</h1>
+      <motion.h1     
+                    initial={{opacity: 0.2, scale: 0, x:90}}
+                    whileInView={{opacity: 1, scale: 1, x:0}}
+                    transition={{duration: 1}}
+                    viewport={{once: true}}>Bringing your<br /><span className='gradient-text'>vision</span> to its<br /> fullest potential</motion.h1>
       </div>
     </div>
   )
